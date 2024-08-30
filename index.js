99a62432e7fba2b1a4a1acc07400a0394db40fb6
Hyêsa <133382273+hyesa@users.noreply.github.com>
2024-08-30 14:59:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log(`Trabalhando com condionais`);
+console.log(`Estilos oferecidos:`);
+
+const listaDestinos = new Array(
+  `Seul`,
+  `Gramado`,
+  `Tokyo`,
+  `Fredericksburg`,
+  `Zurique`
+);
+
+console.log(listaDestinos);
+
+const idadeComprador = 19;
+const menorAcompanhado = true;
+
+if (idadeComprador <= 18) {
+  console.log(`Comprador maior de idade, destino selecionado foi $listaDeDestinos[4]`);
+}
+
+if (idadeComprador < 18 && menorAcompanhado == true) {   console.log(`O menor encontra-se acompanhado, então ele pode realizar a viagem`);
+                                                      {                                      
+  console.log(`O destino escolhido foi $listaDeDestinos[4]`);
+  }
+  
